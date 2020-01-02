aa64c31d6d893c00ac8395013bb86873be553384
Duc Khang Nguyen <duckhang2210@gmail.com>
2020-01-02 01:47:39 -0600

    create react app and navbar

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <AppNavbar></AppNavbar>
+    </div>
+  );
+}
+
+export default App;
