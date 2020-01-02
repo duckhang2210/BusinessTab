ea3c625e95494046ac756e3dca71022ea4d349ad
Duc Khang Nguyen <duckhang2210@gmail.com>
2020-01-02 03:09:42 -0600

    add RestaurantList component and some fake datas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
+import RestaurantList from './components/RestaurantList';
 
 function App() {
   return (
     <div className='App'>
       <AppNavbar></AppNavbar>
+      <RestaurantList></RestaurantList>
     </div>
   );
 }
