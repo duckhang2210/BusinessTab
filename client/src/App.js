865e0d87764dc0da7cd2e917908d00058edf8cb1
Duc Khang Nguyen <duckhang2210@gmail.com>
2020-01-02 23:58:57 -0600

    add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppNavbar from './components/AppNavbar';
-import RestaurantList from './components/RestaurantList';
+import Restaurants from './components/Restaurants';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <AppNavbar></AppNavbar>
-      <RestaurantList></RestaurantList>
-    </div>
+    <Router>
+      <div>
+        <AppNavbar />
+        <Switch>
+          <Route exact path='/' component={Restaurants} />
+          <Route exact path='/books' component={Restaurants} />
+          <Route exact path='/books/:id' component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Restaurants.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button, Media } from 'reactstrap';
+import API from '../utils/API';
+
+class Restaurants extends Component {
+  state = {
+    restaurants: []
+  };
+
+  componentDidMount() {
+    this.loadRestaurants();
+  }
+
+  loadRestaurants = () => {
+    API.getRestaurants()
+      .then(res => this.setState({ restaurants: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <Container>
+        <ListGroup>
+          {this.state.restaurants.map(restaurant => (
+            <ListGroupItem key={restaurant._id}>
+              <Media>
+                <Media left>
+                  <Media object data-src='holder.js/64x64' alt='placeholder' />
+                </Media>
+                <Media body>
+                  <Media heading>{restaurant.name}</Media>
+                  {/*{restaurant.rating} <br />*/}
+                  {restaurant.address}
+                </Media>
+              </Media>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+export default Restaurants;
